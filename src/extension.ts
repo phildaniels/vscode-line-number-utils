826e0d3d7b1e4267862c2b10d30e73d9b70d14b4
Phillip Daniels <Phillip.Daniels@ensemblehp.com>
2023-09-11 12:27:08 -0600

    Removing comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,19 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { type ExtensionContext, commands, window } from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "vscode-type-preview-shortener" is now active!'
+  );
+  let disposable = commands.registerCommand(
+    "vscode-type-preview-shortener.helloWorld",
+    () => {
+      window.showInformationMessage(
+        "Hello World from vscode-type-preview-shortener!"
+      );
+    }
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-type-preview-shortener" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-type-preview-shortener.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-type-preview-shortener!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
